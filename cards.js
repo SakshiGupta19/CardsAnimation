188f95a4dee56fad1966ba53872d8f4956f4a524
SakshiGupta19 <saakshi19@navgurukul.org>
2021-02-22 16:40:34 +0530

    code added

---
+++ cards.js
@@ -0,0 +1,47 @@
+$(function () {
+    'use strict';
+  $('.cardboxes').click(function(){
+      $('#container').show(); })
+      
+  $('#container').click(function(){
+        $('.spread').show();
+    })
+ $('.card1').click(function(){
+        $('.displaycard1').show();
+    })
+    $('.card2').click(function(){
+        $('.displaycard2').show();
+    })
+    $('.card3').click(function(){
+        $('.displaycard3').show();
+    })
+    $('.card4').click(function(){
+        $('.displaycard4').show();
+    })
+    $('.card5').click(function(){
+        $('.displaycard5').show();
+    })
+    $('.card6').click(function(){
+        $('.displaycard6').show();
+    })
+    $('.card7').click(function(){
+        $('.displaycard7').show();
+    })
+    $('.card8').click(function(){
+        $('.diplayscard8').show();
+    })
+    $('.card9').click(function(){
+        $('.displaycard9').show();
+    })
+    $('.card10').click(function(){
+        $('.displaycard10').show();
+    })
+  })
+
+
+     
+
+$('#container').hover(function() {
+    $(this).toggleClass('part2');
+  });
+
